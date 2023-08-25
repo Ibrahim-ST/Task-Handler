6ffe4352fdd6b0ebac7926c8b0f3d872614b46ef
Md. Ibrahim Siddiquee <ibrahim.siddiqueee@gmail.com>
2023-08-25 18:47:14 +0600

    initiated project with react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Client from "./Components/Client"
+
+
+function App() { 
+
+  return (
+    <>
+       <h3>Here we go</h3>
+       <Client />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Client.jsx
@@ -0,0 +1,23 @@
+
+
+const Client = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <img className="w-20 h-20 rounded-full" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bWFsZSUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" />
+                    <h4>Client Name</h4>
+                </div>
+                <div>
+                    <img src="https://img.freepik.com/free-photo/half-profile-image-handsome-young-caucasian-man-with-good-skin-brown-eyes-black-stylish-hair-stubble-posing-isolated-against-blank-wall-looking-front-him-smiling_343059-4560.jpg" alt="" />
+                    <h4>Sadik Istiak</h4>
+                </div>
+            </div>
+            <div></div>
+            <div></div>
+            
+        </div>
+    );
+};
+
+export default Client;
